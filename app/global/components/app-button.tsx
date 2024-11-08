ee1972c6c644935433b889011d6289dc0365d524
Olena <t.olena@improveit.solutions>
2024-11-08 14:41:58 +0200

    refactor: change LinkComponent prop to component prop in AppButton and AppLink

--- app/global/components/app-button.tsx
+++ app/global/components/app-button.tsx
@@ -1,11 +1,6 @@
 import {LinkProps} from '@remix-run/react';
-
 import {Button, ButtonProps} from '@mui/material';
-
 import {I18nLink} from './i18n-link';
-
-//
-//
 
 export type AppButtonProps = LinkProps & ButtonProps;
 
@@ -15,8 +10,7 @@ export const AppButton: React.FC<AppButtonProps> = ({
   ...props
 }: AppButtonProps) => {
   return (
-    // @ts-expect-error - `LinkOwnProps` is not compatible with `ButtonProps`
-    <Button viewTransition={viewTransition} LinkComponent={I18nLink} {...props}>
+    <Button viewTransition={viewTransition} component={I18nLink} {...props}>
       {children}
     </Button>
   );
--- app/global/components/app-link.tsx
+++ app/global/components/app-link.tsx
@@ -1,11 +1,6 @@
 import {LinkProps} from '@remix-run/react';
-
 import {Link as MuiLink, LinkProps as MuiLinkProps} from '@mui/material';
-
 import {I18nLink} from './i18n-link';
-
-//
-//
 
 type MuiAppI18nLinkProps = LinkProps & Omit<MuiLinkProps, 'href'>;
 
@@ -15,8 +10,7 @@ export const AppLink: React.FC<MuiAppI18nLinkProps> = ({
   ...props
 }: MuiAppI18nLinkProps) => {
   return (
-    // @ts-expect-error - `to` is not allowed in MuiLinkProps
-    <MuiLink viewTransition={viewTransition} LinkComponent={I18nLink} href={props.to} {...props}>
+    <MuiLink viewTransition={viewTransition} component={I18nLink} href={props.to} {...props}>
       {children}
     </MuiLink>
   );
